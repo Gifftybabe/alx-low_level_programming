12cf297d557f336e6d79da68e52524ef5020d171
Gifftybabe <ulokangozi@gmail.com>
2022-09-08 23:53:44 +0100

    Proper grammer commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * * main - prints the size of various types
+ *
+ * * Return: 0 if exited properly, non-zero otherwise
+ *
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
