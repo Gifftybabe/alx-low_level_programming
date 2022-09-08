3609c3019bc811af319400c6f797339a8e152791
Gifftybabe <ulokangozi@gmail.com>
2022-09-08 23:44:26 +0100

    puzzle update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * * main - prints the size of various types
  *
- * Return: Always 0 (successful)
+ * * Return: 0 if exited properly, non-zero otherwise
+ *
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
