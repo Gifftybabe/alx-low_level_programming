571a819751b84f22844cf344f8a77454ddbc4b1d
Gifftybabe <ulokangozi@gmail.com>
2022-09-13 23:32:26 +0100

    Multiple nested commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> checks if the character is lowercase
+ * @c: a character argument
+ * Return: returns 1 and 0 depending on the condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
