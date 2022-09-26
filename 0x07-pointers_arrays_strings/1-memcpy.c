b89de113c6c1d3ea9fc190054879c91275be9ed5
Gifftybabe <ulokangozi@gmail.com>
2022-09-27 00:15:33 +0100

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 		unsigned int a;
 
-	for (a = 0; a < n; a++)
-	dest[a] = src[a];
-	return (dest);
+		for (a = 0; a < n; a++)
+		dest[a] = src[a];
+		return (dest);
 }
