98434dbf72454fb1bcf9f1c9603df0dd323407bc
Gifftybabe <ulokangozi@gmail.com>
2022-09-27 04:22:37 +0100

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.ih"
 
 /**
  * _strpbrk - search a string for any of a set of bytes
@@ -26,6 +26,10 @@ char *_strpbrk(char *s, char *accept)
 
 			b++;
 		}
+
+		a++;
+
+	}
 	return ('\0');
 
 	}
