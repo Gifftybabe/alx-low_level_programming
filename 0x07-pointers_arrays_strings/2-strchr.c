d51790b1a776a09c4a3dff81899d4a91ba1c5e01
Gifftybabe <ulokangozi@gmail.com>
2022-09-27 01:19:46 +0100

    task3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- *   * _strchr - locate character in string
- *     * @s: source string
- *       * @c: character to find
- *         *
- *           * Return: the string from character found
- *             */
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+ **/
 char *_strchr(char *s, char c)
 {
 		int a = 0, b;
 
-			while (s[a])
-					{
-								a++;
-									}
+		while (s[a]);
+		{
+			a++;
+		}				
+		for (b = 0; b <= a; b++)
+		{
+			if (c == s[b])
+			{
+				s += b;
+				return (s);
+																		}
+		}
 
-				for (b = 0; b <= a; b++)
-						{
-									if (c == s[b])
-												{
-																s += b;
-																			return (s);
-																					}
-										}
-
-					return ('\0');
+		return ('\0');
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- *   * _strspn - search a string for a set of bytes
- *     * @s: source string
- *       * @accept: accepted string
- *         *
- *           * Return: number of bytes in the init segment
- *             */
+* _strspn - search a string for a set of bytes
+* @s: source string
+* @accept: accepted string
+*
+* Return: number of bytes in the init segment
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-		unsigned int a = 0, b, t = 0;
+	unsigned int a = 0, b, t = 0;
 
-			while (accept[a])
-					{
-								b = 0;
+	while (accept[a])
+	{
+		b = 0;
 
-										while (s[b] != 32)
-													{
-																	if (accept[a] == s[b])
-																					{
-																										t++;
-																													}
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b]
+			{
+				t++;
+			}
 
-																				b++;
-																						}
+			b++;
+		}
 
-												a++;
-													}
+		a++;
 
-				return (t);
+	}
+		
+		return (t);
 }
+			
