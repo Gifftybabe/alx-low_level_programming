29ed8d7320ce9fb6c713f4fa021aad839ad3d19a
Gifftybabe <ulokangozi@gmail.com>
2022-10-27 12:03:39 -0700

    Initial commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
